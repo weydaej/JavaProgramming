43d1da1d376cbdb3081b7b8039ba901f5c048df1
Emily Weyda <54867682+weydaej@users.noreply.github.com>
2020-03-22 04:02:41 -0400

    completed lab 08

--- NetBeansProjects/GetMethodical/src/PrettyHeader.java
+++ NetBeansProjects/GetMethodical/src/PrettyHeader.java
@@ -19,6 +19,7 @@ public class PrettyHeader {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter a message to print: ");
         String msg = in.nextLine();
+//        String msg = "Returns the substring starting from the specified index i.e beginIndex and extends to the character";
         SafeInput.prettyHeader(msg);
     }
 }
