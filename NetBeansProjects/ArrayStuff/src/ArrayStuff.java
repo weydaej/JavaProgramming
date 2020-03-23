0a1cd34e126ad04a617db1bc4b8e1ea5dc13e0cc
Emily Weyda <54867682+weydaej@users.noreply.github.com>
2020-03-23 17:58:01 -0400

    adding lab 09

---
+++ NetBeansProjects/ArrayStuff/src/ArrayStuff.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author weydaej
+ */
+import java.util.Random;
+public class ArrayStuff {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Random rnd = new Random();
+    }
+    
+}
