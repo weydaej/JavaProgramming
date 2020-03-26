04be6b37979920040d6ec6cdb9c1c30d0ee6d0e3
Emily Weyda <54867682+weydaej@users.noreply.github.com>
2020-03-26 13:51:12 -0400

    added helper functions and javadoc documentation

--- NetBeansProjects/TicTacToe/src/TicTacToe.java
+++ NetBeansProjects/TicTacToe/src/TicTacToe.java
@@ -17,5 +17,56 @@ public class TicTacToe {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         SafeInput.getRangedInt(in, "", 0, 0);
+        
+    }
+    
+    /* clears board */
+    private static void clearBoard() {
+        
+    }
+    
+    /* displays tic tac toe board */
+    private static void display() {
+        
+    }
+    
+    /**
+     * @param row an integer representing the row
+     * @param col an integer representing the column
+     * @return true if valid move
+     */
+    private static boolean isValidMove(int row, int col) {
+        
+    }
+    
+    /**
+     * @param player a string that represents the player
+     * @return true column win condition
+     */
+    private static boolean isColWin(String player) {
+        
+    }
+    
+    /**
+     * @param player a string that represents the player
+     * @return true row win condition
+     */
+    private static boolean isRowWin(String player) {
+        
+    }
+    
+    /**
+     * @param player a string that represents the player
+     * @return true diagonal win condition
+     */
+    private static boolean isDiagonalWin(String player) {
+        
+    }
+    
+    /**
+     * @return true if players tie
+     */
+    private static boolean isTie() {
+        
     }
 }
