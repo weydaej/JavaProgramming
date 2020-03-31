0ac62431feff7a629eb0058b6ab210364b578a81
Emily Weyda <54867682+weydaej@users.noreply.github.com>
2020-03-31 19:30:37 -0400

    changed my mind -- this function is too specific to be reused very often

--- NetBeansProjects/ListMaker/src/ListMaker.java
+++ NetBeansProjects/ListMaker/src/ListMaker.java
@@ -16,18 +16,52 @@ public class ListMaker {
      */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        boolean run = true;
+        
+        String option1 = "Add";
+        String option2 = "Delete";
+        String option3 = "Print";
+        String option4 = "Quit";
+
+        String opt1 = option1.substring(0, 1);
+        String opt2 = option2.substring(0, 1);
+        String opt3 = option3.substring(0, 1);
+        String opt4 = option4.substring(0, 1);
+        System.out.printf("Select a menu option:\n    %s: %s\n    %s: %s\n    %s: %s\n    %s: %s\n", opt1, option1, opt2, option2, opt3, option3, opt4, option4);
         String ans = "";
-        String opt1 = "Open".substring(0, 1);
-        System.out.println(opt1);
+        boolean run = true;
+        do {
+            if (in.hasNext()) {
+                ans = in.nextLine().toUpperCase();
+                if (ans.equals(opt1) || ans.equals(opt2) || ans.equals(opt3) || ans.equals(opt4)) {
+                    run = false;
+                    if (ans.equals(opt4)) {
+                        if (!SafeInput.getYNConfirm(in, "Are you sure")) {
+                            run = true;
+                        } 
+                    }
+                }
+            }
+        } while (run);
+        
+        
+    }
+    
+//    public static String menu(Scanner pipe, String option1, String option2, String option3, String option4) {
+//        String opt1 = option1.substring(0, 1);
+//        String opt2 = option2.substring(0, 1);
+//        String opt3 = option3.substring(0, 1);
+//        String opt4 = option4.substring(0, 1);
+//        System.out.printf("Select a menu option:\n    %s: %s\n    %s: %s\n    %s: %s\n    %s: %s\n", opt1, option1, opt2, option2, opt3, option3, opt4, option4);
+//        String ans = "";
+//        boolean run = true;
 //        do {
-//            System.out.println("Select a menu option:\n    O: Open\n    S: Save\n    V: View\n    Q: Quit");
-//            if (in.hasNext()) {
-//                ans = in.nextLine().toUpperCase();
-//                if (ans.equals("A") || ans.equals("D") || ans.equals("P") || ans.equals("Q")) {
+//            if (pipe.hasNext()) {
+//                ans = pipe.nextLine().toUpperCase();
+//                if (ans.equals(opt1) || ans.equals(opt2) || ans.equals(opt3) || ans.equals(opt4)) {
 //                    run = false;
 //                }
 //            }
 //        } while (run);
-    }
+//        return ans;
+//    }
 }
