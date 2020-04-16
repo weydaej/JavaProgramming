cbafca89d121474a39b4677912281d53bb9dcbd6
Emily Weyda <54867682+weydaej@users.noreply.github.com>
2020-04-15 21:18:38 -0400

    lab12

--- NetBeansProjects/FileAway/src/FileAway.java
+++ NetBeansProjects/FileAway/src/FileAway.java
@@ -20,7 +20,6 @@ public class FileAway {
         // testing out how to use File class
         Path target = new File(System.getProperty("user.dir")).toPath();
         target = target.resolve("src\\test.txt");    
-        System.out.println(target);
         
     }
 }
