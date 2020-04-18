626ecd17569fec1259de8b623e7c36348551d9b3
Emily Weyda <54867682+weydaej@users.noreply.github.com>
2020-04-18 01:50:53 -0400

    lab 13

--- NetBeansProjects/FileListMaker/src/ListFileMaker.java
+++ NetBeansProjects/FileListMaker/src/ListFileMaker.java
@@ -11,6 +11,8 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Path;
 import javax.swing.JFileChooser;
@@ -35,13 +37,21 @@ public class ListFileMaker {
         target = target.resolve("src");
         chooser.setCurrentDirectory(target.toFile());
         
-        
         do {
             ans = printMenu(in, arrList);
             switch (ans) {
                 case "A":
                     addToArrList(in, arrList);
                     needsToBeSaved = true;
+                    try  {
+                        FileWriter writer = new FileWriter("output.txt");
+                        for (String str: arrList) {
+                            writer.write(str + System.lineSeparator());
+                        }
+                        writer.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 case "C":
                     needsToBeSaved = true;
@@ -51,6 +61,19 @@ public class ListFileMaker {
                     deleteFromArrList(in, arrList);
                     break;
                 case "O":
+                    try {
+                        if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                            target = chooser.getSelectedFile().toPath();
+                            inFile = new Scanner(target);
+                            System.out.println("File: " + target.getFileName());
+                        } else { // user did not select a file
+                            System.out.println("You must select a file! Terminating program...");
+                            System.exit(0);
+                        }
+                    } catch (IOException e) {
+                        System.out.println("IOException Error");
+                        e.printStackTrace();
+                    }
                     break;
                 case "S":
                     break;
@@ -86,9 +109,6 @@ public class ListFileMaker {
         arrList.remove(itemToDelete - 1);
     }
     
-    public static void openListFromFile() {
-        
-    }
     
     public static void saveCurrentFile() {
         
